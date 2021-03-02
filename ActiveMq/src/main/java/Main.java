3c272eb2d83608998da9dc104a8d030c04121188
Pavel Mironov <35669218+slokov@users.noreply.github.com>
2021-03-02 17:23:38 +0300

    Замена директории с классами

---
+++ ActiveMq/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+import javax.jms.JMSException;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+public class Main {
+
+	public static void main(String[] args) throws JMSException, ParserConfigurationException, SAXException, IOException {
+		
+		mqReceiver receiverOne = new mqReceiver();
+		receiverOne.start();
+		
+		
+	}
+
+}
